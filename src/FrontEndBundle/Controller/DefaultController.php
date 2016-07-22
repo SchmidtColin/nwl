4da5d28ec5cc3724cdd9d7bbbacbfbc9fa38d829
Guido Eckelt <guido.eckelt@gmail.com>
2016-07-22 14:45:26 +0200

    init

--- src/FrontEndBundle/Controller/DefaultController.php
+++ src/FrontEndBundle/Controller/DefaultController.php
@@ -2,8 +2,6 @@
 
 namespace FrontEndBundle\Controller;
 
-use nwlBundle\Entity\WhiteListRequest;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -23,24 +21,33 @@ class DefaultController extends Controller
      * @Route("/login", name="whitelist-request.login")
      */
     public function loginAction(){
-
+        return $this->render('FrontEndBundle:Default:login.html.twig');
     }
 
     /**
      * @Route("/whitelist-request/{username}", name="whitelist-request.list")
      * @param string $username
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function listAction($username){
+        $params = array('username'=>$username);
+        $template = null ;
+        if($username != null){ //check if user or admin
+            $template = 'FrontEndBundle:Default:userList.html.twig';
+        }else{
+            $template = 'FrontEndBundle:Default:adminList.html.twig';
+        }
 
+        return $this->render($template, $params);
     }
 
     /**
-     * @Route("/whitelist-request/create", name="whitelist-request.form")
+     * @Route("/whitelist-request/{username}/create", name="whitelist-request.form")
      */
-    public function createFormAction()
+    public function createFormAction($username)
     {
-
-
+        return $this->render('FrontEndBundle:Default:requestform.html.twig', array('username'=>$username));
     }
 
 }
